cadc826d0584f54f2d5e3cc014b58344842196a7
Efim Docenko <104095998+EimDoc@users.noreply.github.com>
2026-02-22 14:40:12 +0300

    FiboC done

--- src/by/it/group551001/docenko/lesson01/FiboC.java
+++ src/by/it/group551001/docenko/lesson01/FiboC.java
@@ -6,6 +6,9 @@ package by.it.group551001.docenko.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -13,7 +16,7 @@ public class FiboC {
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
         int n = 55555;
-        int m = 1000;
+        int m = 100;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
@@ -22,11 +25,36 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
-        //Интуитивно найти решение не всегда просто и
-        //возможно потребуется дополнительный поиск информации
-        return -1L;
-    }
+        ArrayList<Long> arr = new ArrayList<>();
+        BigInteger x1, x2, x;
+        long mod_1, mod_2;
 
+        mod_1 = mod_2 = 1L;
+        x1 = x2 = BigInteger.ONE;
+
+        arr.add(0L);
+        arr.add(1L);
+        arr.add(1L);
+
+        if (n == 0)
+            return 0;
+        else if (n < 3)
+            return 1;
+
+        while (mod_1 != 0 || mod_2 != 1) {
+            x = x1.add(x2);
+            x1 = x2;
+            x2 = x;
+
+            mod_1 = mod_2;
+            mod_2 = x.mod(BigInteger.valueOf(m)).longValue();
+
+            arr.add(mod_2);
+            System.out.println(x);
+        }
+
+        return arr.get((int) n % (arr.size()-2));
+    }
 
 }
 
