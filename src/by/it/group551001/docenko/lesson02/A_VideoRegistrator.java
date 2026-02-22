c0f32c83b3ee1e4aeaf3b478d2a648341199626a
Efim Docenko <104095998+EimDoc@users.noreply.github.com>
2026-02-22 19:46:16 +0300

    A_VideoRegistrator done

--- src/by/it/group551001/docenko/lesson02/A_VideoRegistrator.java
+++ src/by/it/group551001/docenko/lesson02/A_VideoRegistrator.java
@@ -1,6 +1,7 @@
 package by.it.group551001.docenko.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 Даны события events
@@ -21,24 +22,23 @@ public class A_VideoRegistrator {
 
     //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration) {
-        //events - события которые нужно зарегистрировать
-        //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;
         result = new ArrayList<>();
-        int i = 0;                              //i - это индекс события events[i]
-        //Комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-        //Подготовка к жадному поглощению массива событий
-        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
-        //C*(n log n) + C1*n = O(n log n)
+        int i = 0;
+        double capchured = events[i] + workDuration;
 
-        //пока есть незарегистрированные события
-        //получим одно событие по левому краю
-        //и запомним время старта видеокамеры
-        //вычислим момент окончания работы видеокамеры
-        //и теперь пропустим все покрываемые события
-        //за время до конца работы, увеличивая индекс
+        Arrays.sort(events);
+        while (i < events.length) {
+            if (events[i] > capchured) {
+                result.add(capchured - workDuration);
+                capchured = events[i] + workDuration;
+            }
 
+            i += 1;
+        }
 
-        return result;                        //вернем итог
+        result.add(capchured - workDuration);
+
+        return result;
     }
 }
